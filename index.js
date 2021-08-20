0a62f7e1f0f2b73a467bfd5dff78f45f383185c0
Diego Castro <diegojcr4@gmail.com>
2021-08-20 14:58:15 -0400

    added sum a to b function

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  let total = fromN;
+  if (fromN < toN) {
+    total += sum(fromN+1, toN);
+  }
+  return total;
 }
 
 module.exports = sum;
